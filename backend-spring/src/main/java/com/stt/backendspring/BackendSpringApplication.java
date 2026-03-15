72a638483875372eaa846817e77d6eb0d4878b0b
SouthSun425 <namtaeyang@gmail.com>
2026-03-15 20:36:13 +0900

    restore: backend spring original application

--- backend-spring/src/main/java/com/stt/backendspring/BackendSpringApplication.java
+++ backend-spring/src/main/java/com/stt/backendspring/BackendSpringApplication.java
@@ -2,20 +2,8 @@ package com.stt.backendspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 
-@SpringBootApplication(
-        scanBasePackages = {
-                "com.stt.backendspring.health"
-        },
-        exclude = {
-                DataSourceAutoConfiguration.class,
-                HibernateJpaAutoConfiguration.class,
-                JpaRepositoriesAutoConfiguration.class
-        }
-)
+@SpringBootApplication
 public class BackendSpringApplication {
 
     public static void main(String[] args) {
